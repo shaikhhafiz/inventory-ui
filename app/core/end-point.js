300c9242cb6afc6f2b165dcfea643b2c6ce665fc
Hafiz Ahmed <rhafizubd@gmail.com>
2020-09-19 09:29:25 +0600

    Replaced catalog service url by api gateway

--- app/core/end-point.js
+++ app/core/end-point.js
@@ -1,5 +1,5 @@
 const ROOT_URL = `http://localhost:`
 const REQUISITION_URL = `${ROOT_URL}4001/service/req-service/`
-const CATALOG_URL = `${ROOT_URL}8082/`
+const CATALOG_URL = `${ROOT_URL}4001/service/cat-service/`
 export const ITEM_REQUISITION = `${REQUISITION_URL}item-requisition`
 export const ITEM = `${CATALOG_URL}items`
