b17bb1aadade6e79027dfc0d285f7ecd39217dac
Hafiz Ahmed <rhafizubd@gmail.com>
2020-09-21 19:54:30 +0600

    Removed extra api call in item function

--- app/components/settings/item/item.js
+++ app/components/settings/item/item.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { fetchItems } from '../../../core/request-util';
 import ItemList from './item-list';
 
 export default function Item() {
-  const [items, setItems] = React.useState([]);
-  const [error, setError] = React.useState(null);
-
-  React.useEffect(() => {
-    fetchItems()
-      .then((items) => {
-        setItems(items)
-        setError(null)
-      })
-      .catch(() => {
-        console.warn('Error fetching items list: ', error)
-        setError(error)
-      })
-  }, [])
-
   return (
     <React.Fragment>
       <div className='card'>
@@ -26,8 +10,7 @@ export default function Item() {
           <Link to='/settings/item/create'>Create</Link>
         </div>
         <div className='card-body'>
-          <ItemList
-            items={items} />
+          <ItemList/>
         </div>
       </div>
     </React.Fragment>
